122d8d69aa783fc9f66e412dcdb1cf72369e3da9
RISHAD-prog <20-43002-1@student.aiub.edu>
2023-05-04 14:16:23 +0600

    adding with new api data

--- the-news-dragon/myapi/index.js
+++ the-news-dragon/myapi/index.js
@@ -3,14 +3,34 @@ const app = express();
 var cors = require("cors");
 const port = process.env.port || 5000;
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 app.use(cors())
 app.get('/', (req, res) => {
     res.send("Dragon is Running");
 })
 
-app.listen(port, () => {
-    console.log("Dragon Api is running")
-})
 app.get("/categories", (req, res) => {
     res.send(categories);
+})
+app.get("/categories/:id", (req, res) => {
+    const categoryId = parseInt(req.params.id);
+    if (categoryId === 0) {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === categoryId);
+        res.send(categoryNews);
+    }
+})
+app.get("/news", (req, res) => {
+    res.send(news);
+})
+app.get("/news/:id", (req, res) => {
+    const newsId = req.params.id;
+    const newsList = news.find(spnews => spnews._id === newsId);
+    res.send(newsList);
+})
+
+app.listen(port, () => {
+    console.log("Dragon Api is running")
 })
